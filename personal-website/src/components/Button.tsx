8b0af45eaf917b1c2d0892ce4653b55d70b1fc84
Powell, Madison Danielle <2021powell.madison@gmail.com>
2025-03-28 22:30:25 -0400

    Tiny Update

--- personal-website/src/components/Button.tsx
+++ personal-website/src/components/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
-    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+    <button type="button" className={"btn btn-" + color} onClick={() => {}}>
       {children}
     </button>
   );
